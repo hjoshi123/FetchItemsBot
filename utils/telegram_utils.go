4b3639ff97934bdc3dae6781bca4eb0c62f26d70
hjoshi123 <joshi19981998@gmail.com>
2020-12-08 18:03:11 +0530

    [Add]: added some beautifications

--- utils/telegram_utils.go
+++ utils/telegram_utils.go
@@ -85,10 +85,10 @@ func GetWordOfTheDay() (string, error) {
 	}
 
 	output += "Here's your word of the day.. \n\n"
-	output += word.Word + "\n"
-	output += word.Definitons[0].Text + "    ."
-	output += word.Definitons[0].PartOfSpeech + "\n\n"
-	output += "Here's an example sentence for you\n\n"
+	output += "<b>" + word.Word + "</b>" + "\n"
+	output += word.Definitons[0].Text + " ("
+	output += word.Definitons[0].PartOfSpeech + ")" + "\n\n"
+	output += "Here's an example sentence for you:\n\n"
 	output += word.Examples[0].Text
 
 	log.Println(output)
